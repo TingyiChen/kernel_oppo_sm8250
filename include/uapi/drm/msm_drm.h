b37bf1e1e6aed1f3bac1932379ba3256114e3a71
Pomelohan <2652609017@qq.com>
2021-05-15 12:33:26 +0800

    drivers: drm: Import Oplus Changes
    Signed-off-by: Pomelohan <2652609017@qq.com>
    Signed-off-by: Isaac Chen <tingyi364@gmail.com>

--- include/uapi/drm/msm_drm.h
+++ include/uapi/drm/msm_drm.h
@@ -27,6 +27,10 @@
 
 #include "drm.h"
 #include "sde_drm.h"
+//#if defined(OPLUS_FEATURE_PXLW_IRIS5)
+// #Pixelworks@MULTIMEDIA.DISPLAY, 2020/06/02, Pixelworks Iris5
+#include "msm_drm_iris.h"
+//#endif /*OPLUS_FEATURE_PXLW_IRIS5*/
 
 #if defined(__cplusplus)
 extern "C" {
