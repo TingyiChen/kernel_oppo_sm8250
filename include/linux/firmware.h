74c77d5a8d21d5c2024b33f5a2e04615a3afb349
Pomelohan <2652609017@qq.com>
2021-05-15 12:07:05 +0800

    drivers: firmware_loader: Import Oplus Changes
    Signed-off-by: Pomelohan <2652609017@qq.com>

--- include/linux/firmware.h
+++ include/linux/firmware.h
@@ -54,6 +54,12 @@ int request_firmware_into_buf(const struct firmware **firmware_p,
 	const char *name, struct device *device, void *buf, size_t size);
 
 void release_firmware(const struct firmware *fw);
+#ifdef OPLUS_FEATURE_WIFI_BDF
+//Laixin@CONNECTIVITY.WIFI.BASE.HARDWARE.1065227 , 2019/10/17
+//Add for: reload wlan bdf without using cache
+int request_firmware_no_cache(const struct firmware **firmware_p, const char *name,
+       struct device *device);
+#endif /* OPLUS_FEATURE_WIFI_BDF */
 #else
 static inline int request_firmware(const struct firmware **fw,
 				   const char *name,
